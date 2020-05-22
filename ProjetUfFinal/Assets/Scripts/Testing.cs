6ebe7f2adb89876e2b387cb6a458cfa1558f0b81
skynokk <jferro24@orange.fr>
2020-05-22 10:47:04 +0200

    debut du projet, grid

---
+++ ProjetUfFinal/Assets/Scripts/Testing.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+
+public class Testing : MonoBehaviour
+{
+    private Grid grid;
+
+    private void Start()
+    {
+        grid = new Grid(10, 6, 20f, new Vector3(-100f, -20));
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            grid.SetValue(UtilsClass.GetMouseWorldPosition(), 56);
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log(grid.GetValue(UtilsClass.GetMouseWorldPosition()));
+        }
+    }
+}
